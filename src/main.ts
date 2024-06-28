712205364686267055500457fa700376bb28f069
Toshit Chawda <r58playz@gmail.com>
2024-06-28 08:21:19 -0700

    reformat & disable requiring udp

--- src/main.ts
+++ src/main.ts
@@ -3,71 +3,72 @@ import epoxy from "@mercuryworkshop/epoxy-tls";
 //@ts-expect-error typescript doesnt follow the npm exports for some reason
 import ROOTS from "@mercuryworkshop/epoxy-tls/certs"
 export class EpoxyClient implements BareTransport {
-  canstart = true;
-  epxclient: Awaited<ReturnType<any>>["EpoxyClient"]["prototype"] = null!;
-  wisp: string;
+	canstart = true;
+	epxclient: Awaited<ReturnType<any>>["EpoxyClient"]["prototype"] = null!;
+	wisp: string;
 
-  constructor({ wisp }) {
-    this.wisp = wisp;
-  }
-  async init() {  
-    const { EpoxyClient, EpoxyClientOptions } = await epoxy();
+	constructor({ wisp }) {
+		this.wisp = wisp;
+	}
+	async init() {
+		const { EpoxyClient, EpoxyClientOptions } = await epoxy();
 
-    let options = new EpoxyClientOptions();
-	  options.user_agent = navigator.userAgent;
-    this.epxclient = await new EpoxyClient(this.wisp, ROOTS, options);
+		let options = new EpoxyClientOptions();
+		options.user_agent = navigator.userAgent;
+		options.udp_extension_required = false;
+		this.epxclient = await new EpoxyClient(this.wisp, ROOTS, options);
 
-    this.ready = true;
-  }
-  ready = false;
-  async meta() { }
+		this.ready = true;
+	}
+	ready = false;
+	async meta() { }
 
-  async request(
-    remote: URL,
-    method: string,
-    body: BodyInit | null,
-    headers: BareHeaders,
-    signal: AbortSignal | undefined
-  ): Promise<TransferrableResponse> {
-    if (body instanceof Blob)
-      body = await body.arrayBuffer();
+	async request(
+		remote: URL,
+		method: string,
+		body: BodyInit | null,
+		headers: BareHeaders,
+		signal: AbortSignal | undefined
+	): Promise<TransferrableResponse> {
+		if (body instanceof Blob)
+			body = await body.arrayBuffer();
 
-    try {
-      let payload = await this.epxclient.fetch(remote.href, { method, body, headers, redirect: "manual" });
-      return {
-        body: payload.body!,
-        headers: (payload as any).rawHeaders,
-        status: payload.status,
-        statusText: payload.statusText,
-      };
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
+		try {
+			let payload = await this.epxclient.fetch(remote.href, { method, body, headers, redirect: "manual" });
+			return {
+				body: payload.body!,
+				headers: (payload as any).rawHeaders,
+				status: payload.status,
+				statusText: payload.statusText,
+			};
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+	}
 
-  connect(
-    url: URL,
-    origin: string,
-    protocols: string[],
-    requestHeaders: BareHeaders,
-    onopen: (protocol: string) => void,
-    onmessage: (data: Blob | ArrayBuffer | string) => void,
-    onclose: (code: number, reason: string) => void,
-    onerror: (error: string) => void,
-  ): (data: Blob | ArrayBuffer | string) => void {
-    let epsocket = this.epxclient.connect_ws(
-      onopen,
-      onclose,
-      onerror,
-      (data: Uint8Array | string) => data instanceof Uint8Array ? onmessage(data.buffer) : onmessage(data),
-      url.href,
-      protocols,
-      origin,
-    );
+	connect(
+		url: URL,
+		origin: string,
+		protocols: string[],
+		requestHeaders: BareHeaders,
+		onopen: (protocol: string) => void,
+		onmessage: (data: Blob | ArrayBuffer | string) => void,
+		onclose: (code: number, reason: string) => void,
+		onerror: (error: string) => void,
+	): (data: Blob | ArrayBuffer | string) => void {
+		let epsocket = this.epxclient.connect_ws(
+			onopen,
+			onclose,
+			onerror,
+			(data: Uint8Array | string) => data instanceof Uint8Array ? onmessage(data.buffer) : onmessage(data),
+			url.href,
+			protocols,
+			origin,
+		);
 
-    return async (data) => {
-        await epsocket.send(data);
-    }
-  }
+		return async (data) => {
+			await epsocket.send(data);
+		}
+	}
 }
